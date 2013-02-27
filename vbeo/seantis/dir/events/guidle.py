ca930dcb2a849ec08e0e025c1fb2e6fc421d0813
Denis Krienbühl <denis.krienbuehl@gmail.com>
2013-02-27 15:42:10 +0100

    Adds categories for MySwitzerland Tourism

--- vbeo/seantis/dir/events/guidle.py
+++ vbeo/seantis/dir/events/guidle.py
@@ -4,6 +4,8 @@ from zope.interface import Interface
 from seantis.dir.events.interfaces import IEventsDirectory
 from seantis.dir.events.sources.guidle import IGuidleConfig
 
+from vbeo.seantis.dir.events import _
+
 
 class GuidleConfig(grok.MultiAdapter):
     grok.adapts(IEventsDirectory, Interface)
@@ -13,6 +15,22 @@ class GuidleConfig(grok.MultiAdapter):
            "?id=193352638&language=de&dateOption=NA&primaryTreeId=196238674"
            "&tagIds=196238674&where=38421957&sorting=ungrouped"
            "&locationTreeId=13181&template=xml2")
+
+    classification = "Veranstaltungskalender MySwitzerland"
+
+    tags = {
+        'A': _(u"Concerts"),
+        'B': _(u"Theatres"),
+        'C': _(u"Art"),
+        'D': _(u"Fair"),
+        'E': _(u"Customs, Market"),
+        'F': _(u"Festival, Festivities"),
+        'G': _(u"Congress, Course"),
+        'H': _(u"Sports"),
+        'I': _(u"Excursions for guests"),
+        'J': _(u"Meetings, Animation"),
+        'K': _(u"Gastronomy"),
+    }
 
     def __init__(self, context, request):
         self.context = context
